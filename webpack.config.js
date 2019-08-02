bdc653a3e6d44a818cd3648494c0b5ed6645c7a4
Mauricio Fow <mauriciofow@gmail.com>
2019-08-02 10:11:43 -0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+	entry     : {
+		main : path.resolve(__dirname, 'src/index.js')
+	},
+	mode      : 'production',
+	output    : {
+		path     : path.resolve(__dirname),
+		filename : 'js/[name].js'
+	},
+	devServer : {
+		contentBase : './dist',
+		hot         : true,
+		open        : true
+	},
+	module    : {
+		rules : [
+			{
+				test    : /\.(js|jsx)$/,
+				exclude : /node_modules/,
+				use     : [
+					'babel-loader'
+				]
+			},
+			{
+				test : /\.(jpg|png|jpeg|gif)$/,
+				use  : {
+					loader  : 'url-loader',
+					options : {
+						limit : 900
+					}
+				}
+			},
+			{
+				test : /\.css$/,
+				use  : [
+					{
+						loader : 'css-loader'
+					},
+					{ loader: 'style-loader' }
+				]
+			},
+			{
+				test    : /\.m?js$/,
+				exclude : /(node_modules|bower_components)/,
+				use     : {
+					loader  : 'babel-loader',
+					options : {
+						presets : [
+							'@babel/preset-env'
+						]
+					}
+				}
+			}
+		]
+	},
+	plugins   : [
+		new HtmlWebpackPlugin({
+			template : path.resolve(__dirname, 'public/index.html')
+		}),
+		new webpack.DllReferencePlugin({
+			manifest : require('./dist/js/modules_manifest.json')
+		})
+	]
+};
